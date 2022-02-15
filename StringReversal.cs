1e21dfe66af37544a355d8abb35ab4cb04b2500b
NKC11 <98511171+NKC11@users.noreply.github.com>
2022-02-15 01:03:35 -0500

    Create StringReversal.cs

---
+++ StringReversal.cs
@@ -0,0 +1,22 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+   Console.WriteLine("Enter the input");
+    var inputString= Console.ReadLine();
+
+char[] arr= new char[inputString.Length];
+
+int index = inputString.Length-1;
+for (int i=0; i<inputString.Length;i++)
+{
+  arr[i]=inputString[index];
+  index--;
+}
+Console.WriteLine("Reverse of the entered String is");
+foreach(char c in arr)
+{
+  Console.Write(c);
+}
+  }
+}
